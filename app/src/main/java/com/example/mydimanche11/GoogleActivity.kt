556da8258da26e42023016d832ef5d7a18ff63ba
= <andre.cloutier.4@ulaval.ca>
2024-02-15 18:06:51 -0500

    avect google

---
+++ app/src/main/java/com/example/mydimanche11/GoogleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mydimanche11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google)
+    }
+}
